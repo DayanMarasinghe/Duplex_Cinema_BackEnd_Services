ef947870713fbe60364755f5bb4397f17561560e
Dayan Marasinghe <dayanmarasinghe@gmai.com>
2022-04-28 22:42:19 +0530

    post endpoint customer

--- model/customer.js
+++ model/customer.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose')
 
 const customerSchema = new mongoose.Schema({
-
+    username:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: String,
+        required: true
+    },
+    cart:{
+        type:Object,
+        required:true
+    }
 })
 
 module.exports = mongoose.model('Customer', customerSchema)
